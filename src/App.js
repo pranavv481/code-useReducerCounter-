6396e50f3ba5902668d25d6b0df37b9e3e674608
pranavv481 <pranavv481@gmail.com>
2020-05-27 09:04:56 +0530

    useReducer

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import './App.css';
+import CounterOne from './components/CounterOne';
+
+function App() {
+  return (
+    <div className="App">
+      <CounterOne />
+    </div>
+  );
+}
+
+export default App;
